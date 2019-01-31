1f7c23eb9a54664c6785b1007ce8520bb4f38ee2
Augusto Mantilla <aug.ornelas@gmail.com>
2019-01-31 12:17:15 +0000

    Adds stable melpa

--- init.el
+++ init.el
@@ -6,8 +6,8 @@
 (setq package-archives '(("ELPA"  . "http://tromey.com/elpa/")
 			 ("gnu"   . "http://elpa.gnu.org/packages/")
 			 ("melpa" . "https://melpa.org/packages/")
-			 ("org"   . "https://orgmode.org/elpa/")
-			 ("SC"    . "http://joseito.republika.pl/sunrise-commander/")))
+			 ("melpa-stable" . "https://stable.melpa.org/packages/")
+			 ("org"   . "https://orgmode.org/elpa/"))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
